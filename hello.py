ce47622d72cd2c2e17a7dbfed58820d4f5946b6e
Dennis Martin <dcmartin@denkri.com>
2020-07-19 19:02:11 -0500

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+# clear the terminal screen
+import os
+os.system('clear')
+
+############################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+
+
+first_name = 'Dennis'
+age = 70
+fav_pizza = {
+	'Kris': 'Pepperoni',
+	'Dennis': 'Mushroom',
+	'Danno': 'Cheese'
+	}
+
+name = True
+
+print(name)
